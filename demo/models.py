09e69d038909e05d484507d5638f167a637ba587
Denis Vitez <denis@nesteh.com>
2023-07-11 22:57:58 +0200

    Add model for holding table column definitions

--- demo/models.py
+++ demo/models.py
@@ -8,3 +8,21 @@ class DynamicTable(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class DynamicTableColumn(models.Model):
+    SUPPORTED_TYPES = [
+        ("BOOLEAN", "boolean"),
+        ("INTEGER", "integer"),
+        ("STRING", "string")
+    ]
+    table = models.ForeignKey(DynamicTable, on_delete=models.CASCADE)
+    name = models.CharField("Name of dynamic table column", max_length=30)
+    type = models.CharField(
+        max_length=10,
+        choices=SUPPORTED_TYPES,
+        default="STRING"
+    )
+
+    def __str__(self):
+        return self.name
